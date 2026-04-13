b8af0fa5570375cbe22de0ad76142ac84e26728c
peter-kim <198058174+peter-rg@users.noreply.github.com>
2026-04-14 00:31:16 +0300

    'Add database validation for name and number'

--- part3/phonebook-backend/models/contacts.js
+++ part3/phonebook-backend/models/contacts.js
@@ -2,14 +2,28 @@ const mongoose = require('mongoose')
 
 const MONGO_URL = process.env.MONGODB_URL
 
-console.log("connecting to", MONGO_URL)
+console.log('connecting to', MONGO_URL)
 mongoose.connect(MONGO_URL)
-  .then(result => console.log("connected to MongoDB"))
+  .then(() => console.log('connected to MongoDB'))
   .catch(err=> console.log('error connecting to MongoDB', err.message))
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: [true, 'Username is required']
+  },
+  number: {
+    type:String,
+    minLength: 8,
+    validate: {
+      validator:  function(v){
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number! \nit should be formed of two parts that are separated by -, the first part has two or three numbers and the second part also consists of numbers`
+    },
+    required: [true, 'User phone number required!']
+  }
 })
 
 personSchema.set('toJSON', {
