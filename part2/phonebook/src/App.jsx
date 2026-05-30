cad6879475b1c13ff6e1435c9f5fbbbf468c2f11
peter-kim <198058174+peter-rg@users.noreply.github.com>
2026-05-30 21:34:07 +0300

    feat(part2d): implement contact update and delete using promises (EX 2.12-2.15)

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import axios from "axios"
+import phoneServices from "./services/phoneServices"
 
 function App() {
   const [persons, setPersons] = useState([])
@@ -11,35 +11,66 @@ function App() {
   const handleContact =(e)=>setNewContact(e.target.value)
   const handleFilterName =(e)=>setFilterName(e.target.value)
   
+  const removePerson =(id)=>{
+    const name = persons.find(p=>p.id === id).name
+    // console.log("name", name);
+    
+    window.confirm(`Delete ${name}?`) && phoneServices
+      .remove(id)
+      .then(()=>setPersons(persons.filter(p=> p.id !==id)))
+  }
+
+  const formReset =()=>{
+    setNewName('')
+    setNewContact('')
+  }
   const addContact =(e)=>{
     e.preventDefault()
-    const present = persons.findIndex(person =>person.name === newName )
-    if (present < 0){
-      setPersons(persons.concat({name: newName, contact: newContact}))
-    }else{
-    alert(`${newName} is already added in phonebook`)
-    }
+    const existingContant = persons.find(person =>person.name === newName)
 
-    // console.log("name", newName)
-    setNewName('')
-    console.log("contact", newContact);
-    setNewContact('')
-    console.log();
-    
+    if (!existingContant){
+      const createdContact = {
+        name: newName,
+        contact: newContact
+      }
+      phoneServices
+        .create(createdContact)
+        .then(returnedContact =>{
+          setPersons(prevpersons=>prevpersons.concat(returnedContact)),
+          formReset()
+        })
+    }else{
+      const updatedContact = {...existingContant, contact:newContact}
+      const id = existingContant.id
+      const confirmUpdate = window.confirm(
+        `${existingContant.name} already exists. replace the old number with new one`
+      ) 
+      if(confirmUpdate){
+        phoneServices
+        .update(id, updatedContact)
+        .then(changedContact => {
+          setPersons(prevpersons =>prevpersons.map(person=>{
+            return person.id === id? changedContact : person 
+          }),
+          formReset()
+        )})
+      }
+    }   
   }
-    const passedProps= {
+  const passedProps= {
     newName,
     newContact,
     filterName,
     handleName,
     handleContact,
-    handleFilterName
+    handleFilterName,
+    removePerson
   }
 
   useEffect(()=>{
-    axios
-      .get('http://localhost:3000/persons')
-      .then(res=> setPersons(res.data))
+    phoneServices
+      .getAll()
+      .then(persons=> setPersons(persons))
   },[])
   return (
     <div>
@@ -56,8 +87,7 @@ export default App
 
 const Filter =({passedProps})=>{
   // console.log("props", props);
-  const filterName =passedProps.filterName
-  const handleFilterName = passedProps.handleFilterName
+  const {filterName, handleFilterName} = passedProps
   
   return (
     <div>
@@ -68,13 +98,20 @@ const Filter =({passedProps})=>{
 }
 
 const Persons =({passedProps, persons})=>{
-  const filterName = passedProps.filterName
+  const {filterName,removePerson} =passedProps
   const contactsToshow = persons.filter(person=> new RegExp(filterName, "i").test(person.name))
 
   return (
     <div>
       {
-      contactsToshow.map((person,index)=><p key={index}>{person.name} {person.contact}</p>)
+      contactsToshow.map((person)=>{
+        return(
+            <li key={person.id}>
+              <p>{person.name} {person.contact}</p>
+              <button onClick={()=>removePerson(person.id)}>Delete</button>
+            </li>
+        )}
+      )
       }
       {contactsToshow.length ===0 && <p>There is no name with the string <strong>{filterName}</strong></p>}
     </div>
---
+++ part2/phonebook/src/services/phoneServices.js
@@ -0,0 +1,20 @@
+import axios from "axios"
+
+const baseUrl = "http://localhost:3000/persons"
+const create = async(newObject)=>{
+    const res = await axios.post(baseUrl, newObject)
+    return res.data
+}
+const remove = async(id)=>{
+    const res = await axios.delete(`${baseUrl}/${id}`)
+}
+const update = async(id,newObject)=>{
+    const res = await axios.put(`${baseUrl}/${id}`, newObject)
+    return res.data
+}
+
+const getAll = async()=>{
+    const res = await axios.get(baseUrl)
+    return res.data
+}
+export default {create, remove,update, getAll}
