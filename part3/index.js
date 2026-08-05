0e1751e584fcd1d2dd17a80119b0a7da0701a948
peter-kim <198058174+peter-rg@users.noreply.github.com>
2026-08-05 19:21:30 +0300

    feat: add delete, update, and info routes with centralized error handler

--- part3/index.js
+++ part3/index.js
@@ -40,45 +40,37 @@ app.use(morgan(tinyWithBody))
 app.use(express.static('dist'))
 
 // EX 3.1
-app.get('/api/persons', (req,res)=>{
+app.get('/api/persons', (req,res, next)=>{
 	// EX 3.13
 	Person.find({})
 		.then(contacts => res.status(200).json(contacts))
-		.catch(err=> res.status(500).json(
-			{
-				message: 'Database error'
+		.catch(err=> next(err))
+})
+app.get('/info', (req,res, next)=>{
+	Person.countDocuments({})
+		.then(count=>{
+			res.status(200).send(`Phonebook has info for ${count} people <br/><br/> ${Date()}`)
+		})
+		.catch(err => next(err))
+})
+app.get('/api/persons/:id', (req,res, next)=>{
+	Person.findById(req.params.id)
+		.then(contact=>{
+			if(!contact){
+				return res.status(404).end()
 			}
-		))
-})
-// EX 3.2
-app.get('/info', (req,res)=>{
-	res.send(`Phonebook has info for ${contacts.length} people <br/><br/> ${Date()}`)
-})
-// EX 3.3
-app.get('/api/persons/:id', (req,res)=>{
-	const id = req.params.id
-	const contact = contacts.find(c=> c.id === id)
-
-	if(!contact){
-		return res.status(404).end()
-	}
-
-	res.json(contact)
+			res.status(200).json(contact)
+		})
+		.catch(error => next(error))
 })
 // EX 3.4
-app.delete('/api/persons/:id', (req,res)=>{
-	const id = req.params.id
-	const contact = contacts.find(c=> c.id === id)
-	if(!contact){
-		return res.status(400).json({
-			error: "the contact does not exist"
-		})
-	}
-	contacts = contacts.filter(c=> c.id !==id)
-	res.status(204).end()
+app.delete('/api/persons/:id', (req,res, next)=>{
+	Person.findByIdAndDelete(req.params.id)
+		.then(result => res.status(204).end())
+		.catch(err=> next(err))
 })
 // EX 3.5
-app.post('/api/persons', (req,res)=>{
+app.post('/api/persons', (req,res, next)=>{
 	const contact = req.body
 
 	if(!contact || Object.keys(contact).length === 0){
@@ -113,21 +105,48 @@ app.post('/api/persons', (req,res)=>{
 		// 	})
 		// }
 
+		// When all requirements are met create the contact/person
 	const person = new Person({
 		name: contact.name,
 		number: contact.number
 	})
 	person.save()
 		.then(contact=> res.status(200).json(contact))
-		.catch(err => res.status(500).json(
-			{message: 'An error occured'}
-		))
+		.catch(err => next(err))
+})
+
+// Ex 3.17
+app.put('/api/persons/:id', (req,res,next)=>{
+	const {name, number} = req.body
+	if(!number){
+		return res.status(400).json({error: "number is missing"})
+	}
+	const person ={name, number}
+	// Update directly using findByIdAndUpdate
+  // { new: true } returns the updated document instead of the old one
+	Person.findByIdAndUpdate(req.params.id, person, {new: true})
+		.then(updatedContact=>{
+			if(updatedContact){
+				return res.status(200).json(updatedContact)
+			}else{
+				return res.status(404).json({error: "person not found"})
+			}
+		})
+		.catch(err=> next(err))
 })
 
 // incorrect url is redirected to homepage
 app.get('/{*splat}', (req,res)=>{
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 })
+const errorHandler = (error, req, res, next)=>{
+	console.error(error.message)
+	if(error.name === "CastError"){
+		return res.status(400).send({error: "Malformatted id"})
+	}
+	next(error)
+}
+app.use(errorHandler)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
 	console.log(`Serer running on port ${PORT}`)
